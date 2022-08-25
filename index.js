949de97d672d9cc365fb928d9d3e6045c50f784a
Abhinav Kumar <abhin1509@gmail.com>
2022-08-25 14:13:47 +0530

    implemented razorpay backend integration

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const Razorpay = require("razorpay");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hi");
+});
+
+app.post("/order", async (req, res) => {
+  const amount = req.body.amount;
+
+  var instance = new Razorpay({
+    key_id: process.env.key_id,
+    key_secret: process.env.key_secret,
+  });
+
+  const options = {
+    amount: amount * 100, // amount is the smallest currency unit
+    currency: "INR",
+    receipt: "receipt#1",
+    notes: {
+      key1: "value3",
+      key2: "value2",
+    },
+  };
+
+  const myOrder = await instance.orders.create(options);
+
+  res.status(201).json({
+    success: true,
+    amount,
+    order, myOrder
+  });
+});
+
+app.listen(PORT, () => console.log(`Server is running at port ${PORT}...`));
