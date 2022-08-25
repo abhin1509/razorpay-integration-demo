7c179438d1fe6bb0317c17ccd128858b5926cdd5
Abhinav Kumar <abhin1509@gmail.com>
2022-08-25 15:28:28 +0530

    fix bug in backend

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
+require("dotenv").config();
 const express = require("express");
 const Razorpay = require("razorpay");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const key_id = process.env.key_id;
+const key_secret = process.env.key_secret;
 
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("hi");
-});
+app.use(express.static("./public"));
 
 app.post("/order", async (req, res) => {
   const amount = req.body.amount;
 
-  var instance = new Razorpay({
-    key_id: process.env.key_id,
-    key_secret: process.env.key_secret,
-  });
+  var instance = new Razorpay({ key_id, key_secret });
 
   const options = {
     amount: amount * 100, // amount is the smallest currency unit
     currency: "INR",
     receipt: "receipt#1",
-    notes: {
-      key1: "value3",
-      key2: "value2",
-    },
   };
 
   const myOrder = await instance.orders.create(options);
@@ -33,7 +26,7 @@ app.post("/order", async (req, res) => {
   res.status(201).json({
     success: true,
     amount,
-    order: myOrder
+    order: myOrder,
   });
 });
 
